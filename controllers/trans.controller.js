1e2329f091c5c34ce1b97cf90ccde3d799d04b55
Glitch (push-trans) <none>
2020-12-02 14:13:31 +0000

    🐧🍲 Checkpoint ./routers/trans.router.js:25419550/561 ./views/transupdate.pug:25419550/454 ./views/trans.pug:25419550/610 ./views/transcreate.pug:25419550/965 ./controllers/trans.controller.js:25419550/2750

--- controllers/trans.controller.js
+++ controllers/trans.controller.js
@@ -1,32 +1,53 @@
 var db = require("../db");
 const shortid = require("shortid");
+
 module.exports.trans = function (req, res) {
 	var trans = db.get('trans').value();
 	var users = db.get('user').value();
 	var books = db.get('books').value();
-
 	var result = [];
 
-	trans.map(function(item){
+	// { 
+	//	 id: 'transactionId',
+	//   userId: 'userId',
+	//   bookId: 'bookId',
+	//   isComplete: false,
+	// }
+	trans.map(function(item){ // reduce
 		var obj = {};   
 		obj.id = item.id;
 
-		users.map(function(user){
+		users.map(function(user){ // find
 			if(item.userId === user.id){
 				return obj.user = user;
 			}
 		});
 
-		books.map(function(book){
+		books.map(function(book){ // find
 			if(item.bookId === book.id){
 				return obj.book = book;
 			}
 		});
+		obj.isComplete = item.isComplete
 		return result.push(obj)
 	});
-
+	console.log(result);
+	/**{
+		id: 'transactionId',
+		user: {
+		    id: 'userId'
+		    name: 'hoang',
+		},
+		book: {
+		  id: 'bookId',
+		  title: 'book title'
+	  	},
+		  isComplete: false
+		} 
+	*/
 	res.render('trans', {
-		transactions: result
+		transactions: result,
+
 	})
 };
 // thêm userId và BookId mới
@@ -41,7 +62,23 @@ module.exports.create = function(req, res) {
 };
 module.exports.createPost = function(req, res){
 	req.body.id = shortid.generate();
+	// req.body: {
+    //   "userId": "uIdx5Ji1q",
+    //   "bookId": "YcGHF4uwC",
+    //   "id": "k-meYJQmQ"
+    // }
+    // req.body.isComplete = false;
 	db.get('trans').push(req.body).write();  
     res.redirect("/transaction");
 };
+module.exports.idComplete = function (req, res ){
+  var getId = req.params.id;
+  var getData = db
+         .get("trans")
+         .find({ id: getId })
+         .value();
 
+    res.render("transupdate", {
+    trans: getData
+ }); 
+});
--- routers/trans.router.js
+++ routers/trans.router.js
@@ -10,4 +10,31 @@ router.get("/", controller.trans);
 router.get("/create", controller.create);
 
 router.post("/create", controller.createPost);
+
+router.get("/:id/complete", (req, res )=>{
+  var getId = req.params.id;
+  var getData = db
+         .get("trans")
+         .find({ id: getId })
+         .value();
+
+    res.render("transupdate", {
+    trans: getData
+ }); 
+});
+router.post("/:id/complete", (req ,res)=>{
+  var id = req.params.id;
+  var userId = req.body.userId;
+  var bookId  = req.body.bookId;
+  var isComplete= req.body.isComplete
+   
+   db.get('trans')
+  .find({ id: id })
+  .assign({ userId: userId, bookId: bookId,isComplete: isComplete })
+  .write()
+  
+  res.redirect("/transaction");
+})
+
 module.exports = router;
+
